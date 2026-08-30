a69153c6bd1fcd2a81d8c9a0e366c06fb37592e3
Faiz <faizsiddiqui4201@gmail.com>
2026-08-30 19:26:48 +0530

    Fixed fullstack Vercel API routing rules

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,10 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/translate/, ''),
       },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     },
   },
 })
